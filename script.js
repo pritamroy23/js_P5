8f8592200e6213da5ed733cfc268c08139bb15ce
Pritam Roy <roypritam3114@gmail.com>
2025-07-12 23:13:08 +0530

    complete functionality

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+let count =0;
+var valuedisplay =document.getElementById('value')
+var incrementbtn = document.getElementById('inc')
+var decrementbtn = document.getElementById('dec')
+var setinput = document.getElementById('steps')
+var resetbtn = document.getElementById('reset')
+
+incrementbtn. addEventListener('click', function () {
+    let step  = Number(setinput. value);
+    count+= step;
+    valuedisplay.innerHTML = count;
+
+});
+decrementbtn. addEventListener('click', function () {
+    let step  = Number(setinput. value);
+    count-= step;
+    valuedisplay.innerHTML = count;
+
+});
+resetbtn.addEventListener('click', function (){
+    count =0;
+    valuedisplay.innerHTML = count;
+    
+})
